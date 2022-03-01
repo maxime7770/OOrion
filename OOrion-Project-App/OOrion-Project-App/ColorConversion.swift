4b868a2e977092e79ef748fac62fc6bfb3aa3865
Alexandre Barbier <alexandre.barbier@student-cs.fr>
2022-03-01 11:02:00 +0100

    replace if-else cascade with switch/case

--- OOrion-Project-App/OOrion-Project-App/ColorConversion.swift
+++ OOrion-Project-App/OOrion-Project-App/ColorConversion.swift
@@ -6,177 +6,146 @@ func color_conversion (hsv : Array<CGFloat>) -> String {
     let s = hsv[1] * 100
     let v = hsv[2] * 100
     
-    if s <= 10  {
-        if v <= 30 {
+    switch s {
+    case 0...10:
+        switch v {
+        case 0...30:
             return "Noir"
-            }
-        else if v <= 75 {
+        case 30...75:
             return "Gris"
-            }
-        else {
-            return "Blanc"
-            }
-        }
-    if h <= 18 && h >= 0 {
-        let res = "Rouge"
-        if v <= 10 {
+        default:
             return "Noir"
-            }
-        else if v <= 30 {
-            return "Marron"
         }
-        else if  v <= 60 {
-           return res + " foncé"
-            }
-        else {
-            return res
-            }
-        }
-    if h <= 40 && h >= 18 {
-        let res = "Orange"
-        if s < 20 {
-            if v <= 30 {
+    default:
+        switch h {
+        case 0...18:
+            switch v {
+            case 0...10:
                 return "Noir"
-                }
-            else if v <= 80 {
-                return "Gris"
-                }
-            else {
-                return "Blanc"
-                }
-            }
-        else if v <= 10 {
-                return "Noir"
-                }
-            else if v <= 30 {
-                return "Marron foncé"
-                }
-            else if v <= 60 {
+            case 10...30:
                 return "Marron"
+            case 30...60:
+                return "Rouge foncé"
+            default:
+                return "Rouge"
             }
-            else {
-                return res
+        case 18...40:
+            switch s {
+            case 0...20:
+                switch v {
+                case 0...30:
+                    return "Noir"
+                case 30...75:
+                    return "Gris"
+                default:
+                    return "Noir"
+                }
+            default:
+                switch v {
+                case 0...10:
+                    return "Noir"
+                case 10...30:
+                    return "Marron foncé"
+                case 30...60:
+                    return "Marron"
+                default:
+                    return "Orange"
                 }
             }
-    if h <= 65 && h >= 40 {
-        let res = "Jaune"
-        if s < 20 {
-            if v <= 30 {
+        case 40...65:
+            switch s {
+            case 0...20:
+                switch v {
+                case 0...30:
+                    return "Noir"
+                case 39...75:
+                    return "Gris"
+                default:
+                    return "Noir"
+                }
+            default:
+                switch v {
+                case 0...10:
+                    return "Noir"
+                case 10...30:
+                    return "Jaune foncé"
+                default:
+                    return "Jaune"
+                }
+            }
+        case 66...142:
+            switch v {
+            case 0...10:
                 return "Noir"
-                }
-            else if v <= 80 {
-                return "Gris"
-                }
-            else {
-                return "Blanc"
-                }
+            case 10...50:
+                return "Vert foncé"
+            case 50...80:
+                return "Vert"
+            default:
+                return "Vert clair"
             }
-        else if v <= 10 {
+        case 142...175:
+            switch v {
+            case 0...10:
                 return "Noir"
-                }
-            else if v <= 30 {
-                return res + " foncé"
-                }
-            else {
-                return res
-                }
-        }
-    if h <= 142 && h >= 65 {
-        let res = "Vert"
-        if v <= 10 {
-            return "Noir"
+            case 10...30:
+                return "Turquoise foncé"
+            default:
+                return "Turquoise"
             }
-        else if v <= 50 {
-           return res + " foncé"
+        case 175...205:
+            switch v {
+            case 0...10:
+                return "Noir"
+            case 10...50:
+                return "Bleu marine"
+            case 50...80:
+                return "Bleu"
+            default:
+                return "Bleu clair"
             }
-        else if v <= 80 {
-            return res
-        }
-        else {
-            return res + " clair"
+        case 205...265:
+            switch v {
+            case 0...10:
+                return "Noir"
+            case 10...50:
+                return "Bleu marine"
+            case 50...70:
+                return "Bleu foncé"
+            default:
+                return "Bleu"
             }
-        }
-    if h <= 175 && h >= 142 {
-        let res = "Turquoise"
-        if v <= 10 {
-            return "Noir"
+        case 265...295:
+            switch v {
+            case 0...10:
+                return "Noir"
+            case 10...50:
+                return "Violet foncé"
+            default:
+                return "Violet"
             }
-        else if v <= 30 {
-           return res + " foncé"
+        case 295...335:
+            switch v {
+            case 0...10:
+                return "Noir"
+            case 10...50:
+                return "Rose foncé"
+            default:
+                return "Rose"
             }
-        else {
-            return res
+        case 335...360:
+            switch v {
+            case 0...10:
+                return "Noir"
+            case 10...30:
+                return "Marron"
+            case 30...60:
+                return "Rouge foncé"
+            default:
+                return "Rouge"
             }
-        }
-    if h <= 205 && h >= 175 {
-        let res = "Bleu"
-        if v <= 10 {
-            return "Noir"
-        }
-        else if v <= 50 {
-            return res + " marine"
-        }
-        else if v <= 80 {
-            return res
-        }
-        else {
-            return res + " clair"
+        default:
+            return ""
         }
     }
-    if h <= 265 && h >= 205 {
-        let res = "Bleu"
-        if v <= 10 {
-            return "Noir"
-            }
-        else if v <= 50 {
-           return res + " marine"
-            }
-        else if v <= 70 {
-            return res + " foncé"
-        }
-        else {
-            return res
-            }
-        }
-    if h <= 295 && h >= 265 {
-        let res = "Violet"
-        if v <= 10 {
-            return "Noir"
-            }
-        else if v <= 50 {
-           return res + " foncé"
-            }
-        else {
-            return res
-            }
-        }
-    if h <= 335 && h >= 295 {
-        let res = "Rose"
-        if v <= 10 {
-            return "Noir"
-            }
-        else if v <= 50 {
-           return res + " foncé"
-            }
-        else {
-            return res
-            }
-        }
-    if h <= 360 && h >= 335 {
-        let res = "Rouge"
-        if v <= 10 {
-            return "Noir"
-            }
-        else if v <= 30 {
-            return "Marron"
-        }
-        else if  v <= 60 {
-           return res + " foncé"
-            }
-        else {
-            return res
-            }
-        }
-//    print("\(h) \(s) \(v)")
-    return ""
-    }
+}
