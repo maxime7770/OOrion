6002e77950b404976348b9e8299a5370b3d57bfd
Yassine Terrab <yassineterrab@MacBook-Pro-de-Yassine.local>
2022-03-09 15:57:56 +0100

    le script avec les constantes

---
+++ OOrion-Project-App/OOrion-Project-App/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  OOrion-Project-App
+//
+//  Created by Yassine Terrab on 09/03/2022.
+//  Copyright © 2022 Shuichi Tsutsumi. All rights reserved.
+//
+
+import Foundation
